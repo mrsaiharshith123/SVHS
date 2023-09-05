410fded6d3c872f74c1204565a941c296c64130f
mrsaiharshith123 <125900599+mrsaiharshith123@users.noreply.github.com>
2023-09-05 21:37:11 +0530

    Delete main-page directory

--- main-page/script.js
+++
@@ -1,8 +0,0 @@
-function btn() {
-    window.open("https://goo.gl/maps/mAxxNnkxy3CGFGHG8")
-    console.log("done!!")
-}
-function gall() {
-    window.location.href = '/Gallery/gallery.html';
-    console.log("take away!")
-}
