50ac8075e2487e94e5efa0ac0a4de47066c9a24e
mrsaiharshith123 <125900599+mrsaiharshith123@users.noreply.github.com>
2023-09-07 11:42:45 +0530

    updated all files

---
+++ script.js
@@ -0,0 +1,9 @@
+function btn() {
+    window.open("https://goo.gl/maps/mAxxNnkxy3CGFGHG8")
+    console.log("done!!")
+}
+
+function gall(){
+    window.location.href = '/Gallery/gallery.html';
+}
+
